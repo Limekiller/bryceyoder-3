8b3516f28c1260107e8096e5e0751fa74ef97481
Limekiller <me@bryceyoder.com>
2021-08-20 17:17:59 -0400

    Replace Moonami with MoodleUS and update react, next.js versions

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
   }, [])
 
   return (
-    <motion.div 
+    <motion.div
       initial={{opacity: 0, y: 0}}
       animate={{opacity: 1, y: 0, transition: {ease: 'easeInOut', duration: 0.25}}}
       exit={{opacity: 0, y: 0, transition: {ease: 'easeInOut', duration: 0.25}}}
@@ -38,15 +38,15 @@ export default function Home() {
             React, Next.js, Flask, Laravel, WordPress, Moodle, JavaScript, Python, PHP, Java, C#, Ansible, NGINX, Apache, AWS, PhotoShop, Illustrator, InDesign
           </h2>
           <p>
-            Sup dawg. I'm a full-stack developer living in northern Indiana with experience in web development, systems administration, & graphic design. Currently, I'm a Platform Engineer at LMS hosting provider <a href='https://moonami.com'>Moonami</a>. In the past, I have been Lead Developer at grocery-delivery startup <a href='https://www.dropit.bm'>DropIt</a>, Network Technician at LMS hosting provider <a href='https://eclass4learning.com'>eClass4Learning</a>, and System Administrator at WordPress host <a href='https://mennonite.net'>Mennonite.net</a>, and continue to do freelance web design work.
+            Sup dawg. I'm a full-stack developer living in northern Indiana with experience in web development, systems administration, & graphic design. Currently, I'm a DevOps Engineer at <a href='https://moodle.com/us'>MoodleUS</a>. In the past, I have been Lead Developer at grocery-delivery startup <a href='https://www.dropit.bm'>DropIt</a>, Network Technician at LMS hosting provider <a href='https://eclass4learning.com'>eClass4Learning</a>, and System Administrator at WordPress host <a href='https://mennonite.net'>Mennonite.net</a>, and continue to do freelance web design work.
           </p>
           <p>
-            In April 2020 I graduated from <a href='https://goshen.edu'>Goshen College</a> with a Bachelor's in Computer Science and a minor in Graphic Design. I love the problem-solving that comes with programming, but I also have a passion for art and design. So while I greatly enjoy the mathy, algorithm side of the spectrum, I most appreciate front-end programming and the way it merges code and design. 
+            In April 2020 I graduated from <a href='https://goshen.edu'>Goshen College</a> with a Bachelor's in Computer Science and a minor in Graphic Design. I love the problem-solving that comes with programming, but I also have a passion for art and design. So while I greatly enjoy the mathy, algorithm side of the spectrum, I most appreciate front-end programming and the way it merges code and design.
           </p>
         </div>
         <Footer />
       </div>
-      
+
       <style jsx>{`
         .title {
           grid-column: 1 / span 3;
